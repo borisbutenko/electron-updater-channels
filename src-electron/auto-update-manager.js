d198f722c7d1fe801ef00efe30fa9de5d597cbef
borisbutenko <iamborisbutenko@gmail.com>
2019-04-04 13:11:10 +0400

    v0.0.1

--- src-electron/auto-update-manager.js
+++ src-electron/auto-update-manager.js
@@ -1,16 +1,21 @@
+const { autoUpdater, ipcMain } = require('electron')
+const log = require('electron-log')
+const version = require('../package.json').version
+
 module.exports =
 class AutoUpdateManager {
-  constructor (mainWindow, channel = 'latest') {
+  constructor (mainWindow) {
     this.mainWindow = mainWindow
-    this.autoUpdater = require('electron').autoUpdater
-    this.autoUpdater.channel = channel
-    this.version = require('../package.json').version
+    this.autoUpdater = autoUpdater
+    this.ipcMain = ipcMain
+    this.version = version
+    
     this.init()
     this.checkForUpdates()
   }
 
   init () {
-    this.autoUpdater.logger = require('electron-log')
+    this.autoUpdater.logger = log
     this.autoUpdater.logger.transports.file.level = 'info'
 
     this.sendMessageToWindow('console', `App version: ${ this.version }`)
@@ -43,6 +48,8 @@ class AutoUpdateManager {
         `
       })
     })
+
+    this.ipcMain.on('change-channel', (_, value) => this.updateChannel(value))
   }
 
   checkForUpdates () {
@@ -54,8 +61,16 @@ class AutoUpdateManager {
   }
 
   updateChannel (channel) {
-    this.autoUpdater.channel = channel
-    this.checkForUpdates()
+    this.sendMessageToWindow('message', {
+      hide: true,
+      msg: `
+        <p style="margin:0">
+          🤘 Current channel is <b>${ channel }</b>
+        </p>
+      `
+    })
+    // this.autoUpdater.channel = channel
+    // this.checkForUpdates()
   }
 
   sendMessageToWindow (eventName, data) {
--- src-electron/renderer.js
+++ src-electron/renderer.js
@@ -4,14 +4,14 @@ let counter = 0
 
 function showMessage ({ msg, hide = false, replaceAll = false }) {
   let container = document.getElementById('msg-container')
-  let html = `<li id="msg-${ counter++ }">${ msg }</li>`
+  let html = `<li id="msg-${ ++counter }">${ msg }</li>`
 
   if (replaceAll) container.innerHTML = html
   else container.insertAdjacentHTML('beforeend', html)
 
   if (hide) {
     setTimeout(() => {
-      let el = container.getElementById(`${ msg }-${ counter }`)
+      let el = container.querySelector(`#msg-${ counter }`)
       el.remove()
     }, 5000)
   }
@@ -19,6 +19,13 @@ function showMessage ({ msg, hide = false, replaceAll = false }) {
 
 window.quitAndInstall = function quitAndInstall () {
   remote.autoUpdater.quitAndInstall()
+}
+
+window.updateChannel = function updateChannel () {
+  let channels = document.getElementById('channels')
+  let channel = channels[channels.selectedIndex].value
+  
+  ipcRenderer.send('change-channel', channel)
 }
 
 ipcRenderer.on('console', (event, message) => console.info(message))
